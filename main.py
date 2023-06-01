6710fce24c8d921be578880d976b9e6830410517
Paul <paul.eun.shin@gmail.com>
2023-06-01 00:06:12 -0700

    Image searcher start "Cereal Query"

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ import urllib
 import time
 from URL import *
 
-
-
+url = ("https://www.google.com/search?rlz=1C1ONGR_enUS1000US1000&sxsrf=APwXEdew2Gku-ZAmXzNhQ8h_CHX48EJfhQ:1685602588298"
+       "&q=cereal&tbm=isch&sa=X&ved=2ahUKEwiy6If8vqH_AhXMJ0QIHQfjCNUQ0pQJegQICBAB&biw=2560&bih=1297&dpr=1")
+def image_searcher(query):
